c30ccd95604703f9a408ef7b1c6277c848601684
Your Name <lol@mail.com>
2025-05-26 15:05:48 +0200

    Add tests and fix bugs found for stack allocator

--- src/core/arena.c
+++ src/core/arena.c
@@ -1,6 +1,7 @@
 #include "arena.h"
 #include "log.h"
 
+#include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -18,6 +19,22 @@ static size_t calc_alignment_bump(size_t s, size_t alignment) {
   }
   return alignment_bump;
 }
+
+static ptrdiff_t offset_to_aligned(void *ptr, size_t s, size_t alignment) {
+  uintptr_t res = (uintptr_t)(ptr + s) % alignment;
+
+  if (res != 0) {
+    // We want the remaining number of bytes required to align to
+    // 'alignment'
+    res = alignment - res;
+  }
+
+  return res;
+}
+
+/* static ptrdiff_t offset_to_aligned_down(void *ptr, size_t alignment) { */
+/*   return (uintptr_t)ptr % alignment; */
+/* } */
 
 void arena_init(struct ArenaAllocator *allocator, size_t capacity) {
   if (!allocator) {
@@ -59,7 +76,7 @@ void *arena_alloc(struct ArenaAllocator *allocator, size_t element_size,
 
   const size_t count_bytes = element_count * element_size;
 
-  size_t alignment_bump = calc_alignment_bump(alignment, count_bytes);
+  size_t alignment_bump = calc_alignment_bump(count_bytes, alignment);
 
   void *return_ptr = allocator->base + allocator->used + alignment_bump;
   const size_t total_bytes = count_bytes + alignment_bump;
@@ -90,6 +107,12 @@ void arena_dealloc_all(struct ArenaAllocator *allocator) {
   allocator->used = 0;
 }
 
+void arena_free(struct ArenaAllocator *allocator) {
+  free(allocator->base);
+  allocator->capacity = 0;
+  allocator->used = 0;
+}
+
 void stack_init(struct StackAllocator *stack, struct ArenaAllocator *arena,
                 size_t s) {
   // NOTE: Ask for 16 Mi times 1 byte, but align this to max_align_t
@@ -109,35 +132,150 @@ void stack_init(struct StackAllocator *stack, struct ArenaAllocator *arena,
 
 void *stack_alloc(struct StackAllocator *allocator, size_t s,
                   size_t alignment) {
-  size_t alignment_bump = calc_alignment_bump(alignment, s);
-  size_t actual_usage = s + alignment_bump;
-
-  if (actual_usage >= allocator->capacity - allocator->used) {
-    LOG_EXIT("Stack allocation of %zu exceeds capacity %zu", actual_usage,
+  (void)alignment;
+  if (allocator->used + s >= allocator->capacity) {
+    LOG_EXIT("Stack allocation of %zu exceeds capacity %zu", s,
              allocator->capacity);
     return 0;
   }
 
-  // This is the starting point of our allocation of size 's'
-  void *allocation = allocator->base + allocator->used + alignment;
+  void *ptr = allocator->base + allocator->used;
 
-  // But we include the alignment in the actual usage of the allocator
-  allocator->used += actual_usage;
+  if ((uintptr_t)ptr % alignment == 0) {
+    allocator->used += s;
+    return ptr;
+  }
 
-  return allocation;
+  ptrdiff_t offset = offset_to_aligned(ptr, s, alignof(max_align_t));
+  ptr += offset;
+
+  allocator->used += offset + s;
+
+  LOG_INFO("allocated %zu bytes + %zu alignment @ %p", s, offset, ptr);
+  return ptr;
 }
 
-void stack_dealloc(struct StackAllocator *allocator, void *ptr,
-                   size_t alignment, size_t s) {
-  size_t alignment_bump = calc_alignment_bump(alignment, s);
-  if ((ptr + alignment_bump + s) == allocator->base + allocator->used) {
-    allocator->used -= alignment_bump - s;
+int stack_is_most_recent_allocation(struct StackAllocator *allocator, void *ptr,
+                                    size_t s,
+
+                                    ptrdiff_t offset) {
+  if (ptr + s + offset == allocator->base + allocator->used) {
+    return 1;
+  }
+
+  return 0;
+}
+
+void stack_dealloc(struct StackAllocator *allocator, void *ptr, size_t s,
+                   size_t alignment) {
+  ptrdiff_t offset = offset_to_aligned(ptr, s, alignof(max_align_t));
+
+  if (stack_is_most_recent_allocation(allocator, ptr, s, offset)) {
+
+    allocator->used -= s + offset;
   } else {
     LOG_EXIT("Failed to deallocate %x of size %zu with alignment %zu", ptr, s,
              alignment);
   }
 }
 
+int stack_dealloc_checked(struct StackAllocator *allocator, void *ptr, size_t s,
+                          size_t alignment) {
+  ptrdiff_t offset = offset_to_aligned(ptr, s, alignof(max_align_t));
+
+  if (stack_is_most_recent_allocation(allocator, ptr, s, offset)) {
+    allocator->used -= s + offset;
+  } else {
+    LOG_ERROR("Failed to deallocate %x of size %zu with alignment %zu", ptr, s,
+              alignment);
+    return 0;
+  }
+
+  return 1;
+}
+
 void stack_dealloc_all(struct StackAllocator *allocator) {
   allocator->used = 0;
 }
+
+#ifdef BUILD_TESTS
+typedef struct {
+  double d;
+  char c;
+} TestStructPadded;
+
+typedef struct {
+  char array[1024];
+} TestStructLarge;
+
+static void test_offset_to_align() {
+  size_t alignment = alignof(max_align_t);
+  char *ptr = 0;
+
+  for (size_t i = 0; i < alignment; ++i) {
+    uintptr_t offset = (uintptr_t)offset_to_aligned(ptr, i, alignment);
+    uintptr_t expected = (uintptr_t)((alignment - i) % alignment);
+    assert(offset == expected);
+  }
+}
+
+void stack_test() {
+  test_offset_to_align();
+
+  int test_int = 0xf0e0d0c0;
+
+  struct ArenaAllocator arena = {0};
+  arena_init(&arena, 1024 * 1024 * 32);
+
+  struct StackAllocator stack = {0};
+  stack_init(&stack, &arena, 1024 * 1024 * 16);
+
+  int *int_ptr = stack_alloc(&stack, sizeof(int), alignof(int));
+  assert((uintptr_t)int_ptr % alignof(int) == 0);
+  assert(stack.used == 4);
+
+  *int_ptr = 0xf0e0d0c0;
+  TestStructPadded *ts =
+      stack_alloc(&stack, sizeof(TestStructPadded), alignof(TestStructPadded));
+
+  uintptr_t ts_value = (uintptr_t)ts;
+  int alignof_ts = alignof(TestStructPadded);
+  int res = ts_value % alignof_ts;
+  (void)res;
+  assert(ts_value % alignof_ts == 0);
+  // should be aligned to eight and we lose 4 bytes because of previous
+  // allocation of int when allocating TestStructPadded
+  assert(stack.used == 4 + 4 + sizeof(TestStructPadded));
+  assert(stack_dealloc_checked(&stack, int_ptr, sizeof(int), alignof(int)) ==
+         0);
+  assert(stack_dealloc_checked(&stack, ts, sizeof(*ts),
+                               alignof(TestStructPadded)) == 1);
+  assert(*int_ptr == test_int);
+
+  assert(stack_dealloc_checked(&stack, int_ptr, sizeof(int), alignof(int)) ==
+         1);
+  assert(stack.used == 0);
+
+  int *int_ptr_2 = stack_alloc(&stack, sizeof(int), alignof(int));
+  TestStructPadded *ts2 =
+      stack_alloc(&stack, sizeof(TestStructPadded), alignof(TestStructPadded));
+  TestStructLarge *tsl =
+      stack_alloc(&stack, sizeof(TestStructLarge), alignof(TestStructLarge));
+  assert((uintptr_t)int_ptr_2 % alignof(int) == 0);
+  *int_ptr_2 = 1;
+
+  assert(*int_ptr == *int_ptr_2);
+
+  assert(stack_dealloc_checked(&stack, tsl, sizeof(TestStructLarge),
+                               alignof(TestStructLarge)) == 1);
+  assert(stack_dealloc_checked(&stack, ts2, sizeof(TestStructPadded),
+                               alignof(TestStructPadded)) == 1);
+  assert(stack_dealloc_checked(&stack, int_ptr_2, sizeof(int), alignof(int)) ==
+         1);
+
+  assert(stack.used == 0);
+
+  arena_free(&arena);
+}
+
+#endif
--- src/core/arena.h
+++ src/core/arena.h
@@ -18,22 +18,28 @@ void *arena_alloc(struct ArenaAllocator *allocator, size_t element_size,
 
 void arena_dealloc_all(struct ArenaAllocator *allocator);
 
+void arena_free(struct ArenaAllocator *allocator);
+
 // Allocate memory for for 'count' objects of size sizeof('type')
 #define ArenaAlloc(allocator, count, type)                                     \
   arena_alloc(allocator, sizeof(type), count, alignof(type));
 
-// Static life time allocations only 'cause that's
-// really all we support right now :)
-extern struct ArenaAllocator global_static_allocator;
-
-// Things put things here that have "frame" lifetime
-// i.e. can die each frame
-extern struct ArenaAllocator frame_allocator;
+// NOTE: We lose potential bytes used to align the allocation here
+// because we do not know what alignment the allocation required
+// when deallocating again.
+//
+// We could make this a member of a potential Block struct
+// typedef struct {
+//   void* ptr;
+//   size_t s;
+//   size_t aligned_by;
+// };
+// ... But ew?
 
 struct StackAllocator {
-  size_t capacity;
-  size_t used;
   void *base;
+  size_t used;
+  size_t capacity;
 };
 
 void stack_init(struct StackAllocator *stack, struct ArenaAllocator *allocator,
@@ -41,9 +47,30 @@ void stack_init(struct StackAllocator *stack, struct ArenaAllocator *allocator,
 
 void *stack_alloc(struct StackAllocator *allocator, size_t s, size_t alignment);
 
-void stack_dealloc(struct StackAllocator *allocator, void *ptr,
-                   size_t alignment, size_t s);
+// Returns non-null if true
+int stack_is_most_recent_allocation(struct StackAllocator *allocator, void *ptr,
+                                    size_t s, ptrdiff_t offset);
 
+void stack_dealloc(struct StackAllocator *allocator, void *ptr, size_t s,
+                   size_t alignment);
+
+int stack_dealloc_checked(struct StackAllocator *allocator, void *ptr, size_t s,
+                          size_t alignment);
+
+// This is the very definition of dangling pointers. You'd
+// better be sure none of them are in use before allocating
+// again
 void stack_dealloc_all(struct StackAllocator *allocator);
+
+#ifdef BUILD_TESTS
+void stack_test();
+#endif
+
+// Static life time allocations only
+extern struct ArenaAllocator global_static_allocator;
+
+// Things put things here that have "frame" lifetime
+// i.e. can die each frame
+extern struct ArenaAllocator frame_allocator;
 
 #endif
