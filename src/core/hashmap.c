8224e51513f46ea477b9aa56278443d785fa6efb
Your Name <lol@mail.com>
2025-06-06 16:33:41 +0200

    Change hashing function to murmur3, cause mine was seriously just for educational purposes :)

--- src/core/hashmap.c
+++ src/core/hashmap.c
@@ -7,39 +7,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* size_t hash(const void *ptr, size_t len) { */
+/*   const char *data = ptr; */
+/*   const size_t block_size = sizeof(uint32_t); */
+/*   const size_t blocks_end = len - len % sizeof(uint32_t); */
+/*   size_t hash = len; */
+/*   size_t i = 0; */
+/*   for (; i < blocks_end; i += block_size) { */
+/*     size_t tmp = ((data[i] << 24) | (data[i + 1] << 16) | (data[i + 2] << 8)
+ * | */
+/*                   data[i + 3]); */
+
+/*     hash += tmp; */
+/*     tmp = ((data[i]) | (data[i + 1]) | (data[i + 2]) | data[i + 3]); */
+/*     hash ^= tmp; */
+/*   } */
+
+/*   const size_t remainder = len % sizeof(size_t); */
+
+/*   switch (remainder) { */
+/*   case 0: */
+/*   default: */
+/*     break; */
+/*   case 1: */
+/*     hash ^= (data[i] << 24); */
+/*     break; */
+/*   case 2: */
+/*     hash ^= ((data[i] << 24) | (data[i + 1] << 16)); */
+/*     break; */
+/*   case 3: */
+/*     hash ^= ((data[i] << 24) | (data[i + 1] << 16) | (data[i + 2] << 8)); */
+/*     break; */
+/*   } */
+
+/*   return hash; */
+/* } */
+
+static inline uint32_t murmur_32_scramble(uint32_t k) {
+  k *= 0xcc9e2d51;
+  k = (k << 15) | (k >> 17);
+  k *= 0x1b873593;
+  return k;
+}
+
+uint32_t murmur3_32(const uint8_t *key, size_t len, uint32_t seed) {
+  uint32_t h = seed;
+  uint32_t k;
+  /* Read in groups of 4. */
+  for (size_t i = len >> 2; i; i--) {
+    // Here is a source of differing results across endiannesses.
+    // A swap here has no effects on hash properties though.
+    memcpy(&k, key, sizeof(uint32_t));
+    key += sizeof(uint32_t);
+    h ^= murmur_32_scramble(k);
+    h = (h << 13) | (h >> 19);
+    h = h * 5 + 0xe6546b64;
+  }
+  /* Read the rest. */
+  k = 0;
+  for (size_t i = len & 3; i; i--) {
+    k <<= 8;
+    k |= key[i - 1];
+  }
+  // A swap is *not* necessary here because the preceding loop already
+  // places the low bytes in the low places according to whatever endianness
+  // we use. Swaps only apply when the memory is copied in a chunk.
+  h ^= murmur_32_scramble(k);
+  /* Finalize. */
+  h ^= len;
+  h ^= h >> 16;
+  h *= 0x85ebca6b;
+  h ^= h >> 13;
+  h *= 0xc2b2ae35;
+  h ^= h >> 16;
+  return h;
+}
+
 size_t hash(const void *ptr, size_t len) {
-  const char *data = ptr;
-  const size_t block_size = sizeof(uint32_t);
-  const size_t blocks_end = len - len % sizeof(uint32_t);
-  size_t hash = len;
-  size_t i = 0;
-  for (; i < blocks_end; i += block_size) {
-    size_t tmp = ((data[i] << 24) | (data[i + 1] << 16) | (data[i + 2] << 8) |
-                  data[i + 3]);
-
-    hash += tmp;
-    tmp = ((data[i]) | (data[i + 1]) | (data[i + 2]) | data[i + 3]);
-    hash ^= tmp;
-  }
-
-  const size_t remainder = len % sizeof(size_t);
-
-  switch (remainder) {
-  case 0:
-  default:
-    break;
-  case 1:
-    hash ^= (data[i] << 24);
-    break;
-  case 2:
-    hash ^= ((data[i] << 24) | (data[i + 1] << 16));
-    break;
-  case 3:
-    hash ^= ((data[i] << 24) | (data[i + 1] << 16) | (data[i + 2] << 8));
-    break;
-  }
-
-  return hash;
+  return (size_t)murmur3_32((uint8_t *)ptr, len, 0);
 }
 
 void hash_map_init(HashMap *map, unsigned int size) {
