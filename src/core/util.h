7b8daaa826509f98918d896b9072ddcc3ea86c96
Your Name <lol@mail.com>
2025-06-09 16:53:30 +0200

    Make our own ghetto assert macro so we don't have to deal with "unused variable" warnings in tests and such when running tests in release mode

--- src/core/util.h
+++ src/core/util.h
@@ -2,9 +2,17 @@
 #define UTIL_H
 
 #include <stdint.h>
+#include <stdio.h>
 
 #define Min(a, b) (((a) <= (b)) ? (a) : (b))
 #define Max(a, b) (((a) > (b)) ? (a) : (b))
+
+// TODO: __debug_brk and equivalents for non-MSVC
+#define Assert(expr)                                                           \
+  if (!(expr)) {                                                               \
+    printf("Assertion failed %s:%d", __FILE__, __LINE__);                      \
+    exit(-1);                                                                  \
+  }
 
 unsigned char *file_read_all(const char *file_path);
 
